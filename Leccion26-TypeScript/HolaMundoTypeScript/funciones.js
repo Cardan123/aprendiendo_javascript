ea78354c7479e206a18df644428eec2f9d902955
Cardan <cosasdecardan@gmail.com>
2021-07-05 00:35:28 -0500

    Probando Typescript

---
+++ Leccion26-TypeScript/HolaMundoTypeScript/funciones.js
@@ -0,0 +1,14 @@
+var saludo = "HolaMundo desde TypeScript";
+saludo = "cambio";
+var numero;
+numero = 10;
+var cualquiera;
+cualquiera = "cadena";
+cualquiera = 10;
+var PI_NUMERO = 3.14;
+function saludar() {
+    console.log(saludo);
+    console.log(numero);
+    console.log(PI_NUMERO);
+}
+saludar();
